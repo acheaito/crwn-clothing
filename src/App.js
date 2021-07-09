39fa47d303dc808f4db75d5d0ae6bc7b462447eb
Ali Cheaito <ali.cheaito@softvision.com>
2021-07-09 16:21:14 -0400

    Added react-router-dom

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
 import React from 'react';
 import './App.scss';
 
+import { Route, Switch } from 'react-router-dom';
 import HomePage from './pages/homepage/home-page.component';
 
 function App() {
   return (
     <div>
-      <HomePage/>
+      <Switch>  {/*STOP MATCHING AFTER FIRST HIT*/}
+        <Route exact path='/' component={HomePage} /> {/*without exact, partial matches cause component to render */}
+      </Switch>
     </div>
   );
 }
