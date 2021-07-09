e4d48123e0c959c1287084007f64adfccb026374
Ali Cheaito <ali.cheaito@softvision.com>
2021-07-09 16:52:48 -0400

    Navigate withRouter to menu item

--- src/App.js
+++ src/App.js
@@ -4,11 +4,18 @@ import './App.scss';
 import { Route, Switch } from 'react-router-dom';
 import HomePage from './pages/homepage/home-page.component';
 
+const HatsPage = () => (
+  <div>
+    <h1>HATS PAGE</h1>
+  </div>
+)
+
 function App() {
   return (
     <div>
       <Switch>  {/*STOP MATCHING AFTER FIRST HIT*/}
         <Route exact path='/' component={HomePage} /> {/*without exact, partial matches cause component to render */}
+        <Route path='/hats' component={HatsPage} />
       </Switch>
     </div>
   );
