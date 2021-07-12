489fba19a42c797ec9356bfbf3541b6842fae3f6
Ali Cheaito <ali.cheaito@softvision.com>
2021-07-12 14:18:56 -0400

    Added header component

--- src/App.tsx
+++ src/App.tsx
@@ -4,10 +4,12 @@ import './App.scss';
 import { Route, Switch } from 'react-router-dom';
 import HomePage from './pages/homepage/home-page.component';
 import ShopPage from './pages/shop/shop.component';
+import Header from './components/header/header.component';
 
 function App() {
   return (
     <div>
+      <Header />
       <Switch>  {/*STOP MATCHING AFTER FIRST HIT*/}
         <Route exact path='/' component={HomePage} /> {/*without exact, partial matches cause component to render */}
         <Route path='/shop' component={ShopPage} />
