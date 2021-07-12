2bb78b1689c58fbc8246f1b0e18df7a16c538d08
Ali Cheaito <ali.cheaito@softvision.com>
2021-07-12 11:31:03 -0400

    added shop page

--- src/App.js
+++ src/App.js
@@ -3,19 +3,14 @@ import './App.scss';
 
 import { Route, Switch } from 'react-router-dom';
 import HomePage from './pages/homepage/home-page.component';
-
-const HatsPage = () => (
-  <div>
-    <h1>HATS PAGE</h1>
-  </div>
-)
+import ShopPage from './pages/shop/shop.component';
 
 function App() {
   return (
     <div>
       <Switch>  {/*STOP MATCHING AFTER FIRST HIT*/}
         <Route exact path='/' component={HomePage} /> {/*without exact, partial matches cause component to render */}
-        <Route path='/hats' component={HatsPage} />
+        <Route path='/shop' component={ShopPage} />
       </Switch>
     </div>
   );
